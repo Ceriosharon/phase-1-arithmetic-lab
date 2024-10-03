717bc76bc24813261451ca5d4fc1d11ed07432a9
ceriosharon <ccerio73@gmail.com>
2024-10-03 07:10:30 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 31;  
+let num2 = 2;  
+let multiply = num1 * num2; 
 
-// Write your code here
+
+let num3 = 24; 
+let num4 = 5; 
+let mod = num3 % num4;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+
+let numbers = [10, 15, 20, 5]; 
+let max = Math.max(...numbers);
+
+console.log(`Multiply: ${multiply}`);  
+console.log(`Random: ${random}`);      
+console.log(`Mod: ${mod}`);             
+console.log(`Max: ${max}`);     
